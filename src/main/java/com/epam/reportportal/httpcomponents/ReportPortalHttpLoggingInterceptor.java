fd712846f2cde38d12dc1a0fe1fb90ddd1e66b99
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-10-05 17:47:39 +0300

    HttpComponents logger: WIP

--- src/main/java/com/epam/reportportal/httpcomponents/ReportPortalHttpLoggingInterceptor.java
+++ src/main/java/com/epam/reportportal/httpcomponents/ReportPortalHttpLoggingInterceptor.java
@@ -17,24 +17,42 @@
 package com.epam.reportportal.httpcomponents;
 
 import com.epam.reportportal.formatting.AbstractHttpFormatter;
+import com.epam.reportportal.formatting.http.converters.DefaultCookieConverter;
+import com.epam.reportportal.formatting.http.converters.DefaultHttpHeaderConverter;
+import com.epam.reportportal.formatting.http.converters.DefaultUriConverter;
+import com.epam.reportportal.formatting.http.entities.BodyType;
 import com.epam.reportportal.formatting.http.entities.Cookie;
 import com.epam.reportportal.formatting.http.entities.Header;
+import com.epam.reportportal.httpcomponents.support.HttpEntityFactory;
 import com.epam.reportportal.listeners.LogLevel;
-import org.apache.http.*;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.protocol.HttpContext;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class ReportPortalHttpLoggingInterceptor extends AbstractHttpFormatter<ReportPortalHttpLoggingInterceptor>
 		implements HttpRequestInterceptor, HttpResponseInterceptor {
 
+	private final List<Predicate<HttpRequest>> requestFilters = new CopyOnWriteArrayList<>();
+	private final List<Predicate<HttpResponse>> responseFilters = new CopyOnWriteArrayList<>();
+
 	/**
-	 * Create a formatter with the specific log level and converters.
+	 * Create a Logging Interceptor with the specific log level and converters.
 	 *
-	 * @param defaultLogLevel           log level on which OKHTTP3 requests/responses will appear on Report Portal
+	 * @param defaultLogLevel           log level on which Apache HttpComponents requests/responses will appear on
+	 *                                  Report Portal
 	 * @param headerConvertFunction     if you want to preprocess your HTTP Headers before they appear on Report Portal
 	 *                                  provide this custom function for the class, default function formats it like
 	 *                                  that: <code>header.getName() + ": " + header.getValue()</code>
@@ -44,13 +62,12 @@ public class ReportPortalHttpLoggingInterceptor extends AbstractHttpFormatter<Re
 	 * @param uriConverterFunction      the same as 'headerConvertFunction' param but for URI, default function returns
 	 *                                  URI "as is"
 	 */
-	protected ReportPortalHttpLoggingInterceptor(@NotNull LogLevel defaultLogLevel,
+	protected ReportPortalHttpLoggingInterceptor(@Nonnull LogLevel defaultLogLevel,
 			@Nullable Function<Header, String> headerConvertFunction,
 			@Nullable Function<Header, String> partHeaderConvertFunction,
 			@Nullable Function<Cookie, String> cookieConvertFunction,
 			@Nullable Function<String, String> uriConverterFunction) {
-		super(
-				defaultLogLevel,
+		super(defaultLogLevel,
 				headerConvertFunction,
 				partHeaderConvertFunction,
 				cookieConvertFunction,
@@ -58,13 +75,111 @@ public class ReportPortalHttpLoggingInterceptor extends AbstractHttpFormatter<Re
 		);
 	}
 
-	@Override
-	public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+	/**
+	 * Create a Logging Interceptor with the specific log level and converters.
+	 *
+	 * @param defaultLogLevel           log level on which Apache HttpComponents requests/responses will appear on
+	 *                                  Report Portal
+	 * @param headerConvertFunction     if you want to preprocess your HTTP Headers before they appear on Report Portal
+	 *                                  provide this custom function for the class, default function formats it like
+	 *                                  that: <code>header.getName() + ": " + header.getValue()</code>
+	 * @param partHeaderConvertFunction the same as for HTTP Headers, but for parts in Multipart request
+	 * @param cookieConvertFunction     the same as 'headerConvertFunction' param but for Cookies, default function
+	 *                                  formats Cookies with <code>toString</code> method
+	 */
+	public ReportPortalHttpLoggingInterceptor(@Nonnull LogLevel defaultLogLevel,
+			@Nullable Function<Header, String> headerConvertFunction,
+			@Nullable Function<Header, String> partHeaderConvertFunction,
+			@Nullable Function<Cookie, String> cookieConvertFunction) {
+		this(defaultLogLevel,
+				headerConvertFunction,
+				partHeaderConvertFunction,
+				cookieConvertFunction,
+				DefaultUriConverter.INSTANCE
+		);
+	}
 
+	/**
+	 * Create a Logging Interceptor with the specific log level and header converters.
+	 *
+	 * @param defaultLogLevel           log level on which Apache HttpComponents requests/responses will appear on
+	 *                                  Report Portal
+	 * @param headerConvertFunction     if you want to preprocess your HTTP Headers before they appear on Report Portal
+	 *                                  provide this custom function for the class, default function formats it like
+	 *                                  that: <code>header.getName() + ": " + header.getValue()</code>
+	 * @param partHeaderConvertFunction the same as for HTTP Headers, but for parts in Multipart request
+	 */
+	public ReportPortalHttpLoggingInterceptor(@Nonnull LogLevel defaultLogLevel,
+			@Nullable Function<Header, String> headerConvertFunction,
+			@Nullable Function<Header, String> partHeaderConvertFunction) {
+		this(defaultLogLevel, headerConvertFunction, partHeaderConvertFunction, DefaultCookieConverter.INSTANCE);
+	}
+
+	/**
+	 * Create a Logging Interceptor with the specific log level.
+	 *
+	 * @param defaultLogLevel log level on which Apache HttpComponents requests/responses will appear on Report Portal
+	 */
+	public ReportPortalHttpLoggingInterceptor(@Nonnull LogLevel defaultLogLevel) {
+		this(defaultLogLevel, DefaultHttpHeaderConverter.INSTANCE, DefaultHttpHeaderConverter.INSTANCE);
 	}
 
 	@Override
-	public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
+	public void process(HttpRequest request, HttpContext context) {
+		if (requestFilters.stream().anyMatch(f -> f.test(request))) {
+			return;
+		}
+		emitLog(HttpEntityFactory.createHttpRequestFormatter(request,
+				context,
+				uriConverter,
+				headerConverter,
+				cookieConverter,
+				contentPrettiers,
+				partHeaderConverter,
+				bodyTypeMap
+		));
+	}
 
+	@Override
+	public void process(HttpResponse response, HttpContext context) {
+		if (responseFilters.stream().anyMatch(f -> f.test(response))) {
+			return;
+		}
+		if (!response.getEntity().isRepeatable()) {
+			try {
+				BufferedHttpEntity bufferedEntity = new BufferedHttpEntity(response.getEntity());
+				response.setEntity(bufferedEntity);
+			} catch (Exception ignore) {
+				// will be logged on reading attempt inside HttpEntityFactory class
+			}
+		}
+		emitLog(HttpEntityFactory.createHttpResponseFormatter(response,
+				context,
+				headerConverter,
+				cookieConverter,
+				contentPrettiers,
+				bodyTypeMap
+		));
+	}
+
+	public ReportPortalHttpLoggingInterceptor setBodyTypeMap(@Nonnull Map<String, BodyType> typeMap) {
+		this.bodyTypeMap = Collections.unmodifiableMap(new HashMap<>(typeMap));
+		return this;
+	}
+
+	public ReportPortalHttpLoggingInterceptor setContentPrettiers(
+			@Nonnull Map<String, Function<String, String>> contentPrettiers) {
+		this.contentPrettiers = Collections.unmodifiableMap(new HashMap<>(contentPrettiers));
+		return this;
+	}
+
+	public ReportPortalHttpLoggingInterceptor addRequestFilter(@Nonnull Predicate<HttpRequest> requestFilter) {
+		requestFilters.add(requestFilter);
+		return this;
+	}
+
+	public ReportPortalHttpLoggingInterceptor addResponseFilter(@Nonnull Predicate<HttpResponse> responseFilter) {
+		responseFilters.add(responseFilter);
+		return this;
 	}
 }
--- src/main/java/com/epam/reportportal/httpcomponents/support/HttpEntityFactory.java
+++ src/main/java/com/epam/reportportal/httpcomponents/support/HttpEntityFactory.java
@@ -16,6 +16,167 @@
 
 package com.epam.reportportal.httpcomponents.support;
 
+import com.epam.reportportal.formatting.http.HttpFormatter;
+import com.epam.reportportal.formatting.http.HttpRequestFormatter;
+import com.epam.reportportal.formatting.http.HttpResponseFormatter;
+import com.epam.reportportal.formatting.http.entities.BodyType;
+import com.epam.reportportal.formatting.http.entities.Cookie;
+import com.epam.reportportal.formatting.http.entities.Header;
+import com.epam.reportportal.formatting.http.entities.Param;
+import com.epam.reportportal.listeners.LogLevel;
+import com.epam.reportportal.service.ReportPortal;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.http.*;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static com.epam.reportportal.formatting.http.HttpFormatUtils.*;
+import static java.util.Optional.ofNullable;
+
 public class HttpEntityFactory {
 
+	@Nonnull
+	private static Charset getCharset(@Nonnull HttpEntity httpEntity) {
+		return ofNullable(httpEntity.getContentType()).flatMap(h -> toKeyValue(h.getValue()).filter(p -> "charset".equalsIgnoreCase(
+				p.getKey())).findAny()).map(Pair::getValue).map(Charset::forName).orElse(StandardCharsets.UTF_8);
+	}
+
+	@Nullable
+	private static byte[] toBytes(@Nonnull HttpEntity httpEntity) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			httpEntity.writeTo(baos);
+			return baos.toByteArray();
+		} catch (IOException e) {
+			ReportPortal.emitLog("Unable to read HTTP entity: " + ExceptionUtils.getStackTrace(e),
+					LogLevel.WARN.name(),
+					Calendar.getInstance().getTime()
+			);
+			return null;
+		}
+	}
+
+	@Nullable
+	private static String toString(@Nonnull HttpEntity httpEntity, @Nonnull Charset charset) {
+		return ofNullable(toBytes(httpEntity)).map(b -> new String(b, charset)).orElse(null);
+	}
+
+	@Nullable
+	private static String toString(@Nonnull HttpEntity httpEntity) {
+		Charset charset = getCharset(httpEntity);
+		return toString(httpEntity, charset);
+	}
+
+	@Nullable
+	private static List<Param> toParams(@Nonnull HttpEntity httpEntity) {
+		Charset charset = getCharset(httpEntity);
+		String body = toString(httpEntity, StandardCharsets.ISO_8859_1);
+
+		return ofNullable(body).map(b -> b.split("&")).map(elements -> Arrays.stream(elements).map(element -> {
+			String[] param = element.split("=", 2);
+			try {
+				if (param.length > 1) {
+					return new Param(URLDecoder.decode(param[0], charset.name()),
+							URLDecoder.decode(param[1], charset.name())
+					);
+				}
+				return new Param(URLDecoder.decode(param[0], charset.name()), "");
+			} catch (UnsupportedEncodingException ignore) {
+			}
+			return null;
+		}).filter(Objects::nonNull).collect(Collectors.toList())).orElse(Collections.emptyList());
+	}
+
+	@Nonnull
+	public static HttpFormatter createHttpRequestFormatter(@Nonnull HttpRequest request, @Nonnull HttpContext context,
+			@Nullable Function<String, String> uriConverter, @Nullable Function<Header, String> headerConverter,
+			@Nullable Function<Cookie, String> cookieConverter,
+			@Nullable Map<String, Function<String, String>> contentPrettiers,
+			@Nullable Function<Header, String> partHeaderConverter, @Nonnull Map<String, BodyType> bodyTypeMap) {
+		HttpRequestFormatter.Builder builder = new HttpRequestFormatter.Builder(request.getRequestLine().getMethod(),
+				ofNullable(context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST)).orElse("") + request.getRequestLine()
+						.getUri()
+		);
+		ofNullable(request.getAllHeaders()).ifPresent(headers -> Arrays.stream(headers)
+				.filter(h -> !isCookie(h.getName()))
+				.forEach(h -> builder.addHeader(h.getName(), h.getValue())));
+		ofNullable(request.getAllHeaders()).ifPresent(headers -> Arrays.stream(headers)
+				.filter(h -> isCookie(h.getName()))
+				.flatMap(h -> toKeyValue(h.getValue()))
+				.forEach(h -> builder.addCookie(h.getKey(), h.getValue())));
+		builder.uriConverter(uriConverter)
+				.headerConverter(headerConverter)
+				.cookieConverter(cookieConverter)
+				.prettiers(contentPrettiers);
+
+		if (!(request instanceof HttpEntityEnclosingRequest)) {
+			return builder.build();
+		}
+
+		HttpEntity httpEntity = ((HttpEntityEnclosingRequest) request).getEntity();
+		if (httpEntity == null) {
+			return builder.build();
+		}
+
+		String contentType = ofNullable(httpEntity.getContentType()).map(NameValuePair::getValue).orElse(null);
+		String type = getMimeType(contentType);
+		BodyType bodyType = getBodyType(contentType, bodyTypeMap);
+		switch (bodyType) {
+			case TEXT:
+				builder.bodyText(type, toString(httpEntity));
+				break;
+			case FORM:
+				builder.bodyParams(toParams(httpEntity));
+
+		}
+		return builder.build();
+	}
+
+	@Nonnull
+	public static HttpFormatter createHttpResponseFormatter(@Nonnull HttpResponse response,
+			@Nonnull HttpContext context, @Nullable Function<Header, String> headerConverter,
+			@Nullable Function<Cookie, String> cookieConverter,
+			@Nullable Map<String, Function<String, String>> contentPrettiers,
+			@Nonnull Map<String, BodyType> bodyTypeMap) {
+		StatusLine statusLine = response.getStatusLine();
+		HttpResponseFormatter.Builder builder = new HttpResponseFormatter.Builder(statusLine.getStatusCode(),
+				statusLine.getProtocolVersion().toString() + " " + statusLine.getStatusCode() + " "
+						+ statusLine.getReasonPhrase()
+		);
+		ofNullable(response.getAllHeaders()).ifPresent(headers -> Arrays.stream(headers)
+				.filter(h -> !isSetCookie(h.getName()))
+				.forEach(h -> builder.addHeader(h.getName(), h.getValue())));
+		ofNullable(response.getAllHeaders()).ifPresent(headers -> Arrays.stream(headers)
+				.filter(h -> isSetCookie(h.getName()))
+				.forEach(h -> builder.addCookie(toCookie(h.getValue()))));
+		builder.headerConverter(headerConverter).cookieConverter(cookieConverter).prettiers(contentPrettiers);
+
+		HttpEntity httpEntity = response.getEntity();
+		if (httpEntity == null) {
+			return builder.build();
+		}
+
+		String contentType = ofNullable(httpEntity.getContentType()).map(NameValuePair::getValue).orElse(null);
+		String type = getMimeType(contentType);
+		BodyType bodyType = getBodyType(contentType, bodyTypeMap);
+		if (BodyType.TEXT == bodyType) {
+			builder.bodyText(type, toString(httpEntity));
+		} else {
+			builder.bodyBytes(type, toBytes(httpEntity));
+		}
+		return builder.build();
+	}
 }
